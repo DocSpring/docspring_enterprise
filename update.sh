b9bba4c13fe8046568a423e849f65f355660133a
Nathan Broadbent <git@ndbroadbent.com>
2018-05-29 23:52:05 +0700

    Add update.sh script

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "=> Creating new build..."
+RELEASE_ID=$(convox build --id)
+
+echo "=> Running ./scripts/pre-release in: $RELEASE_ID"
+convox run --release "$RELEASE_ID" worker ./scripts/pre-release
+
+echo "=> Promoting Release: $RELEASE_ID"
+convox releases promote "$RELEASE_ID"
